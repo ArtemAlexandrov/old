06394ac68080644e44b720d46f4b590e00608a01
 Artem Alexandrov <svarog278@gmail.com>
2017-03-20 19:10:55 +0300

    cache

--- lib/smartcore/client/client_auth_methods.rb
+++ lib/smartcore/client/client_auth_methods.rb
@@ -7,23 +7,18 @@ module Smartcore
     end
 
     def sign_in(login, password)
-      Rails.cache.delete(:api_token) if Rails.cache
-      self.token = Smartcore::ClientSignInRequest.new(login: login, password: password).execute
+      self.token = (Rails.cache && Rails.cache.read(:api_token)) || Smartcore::ClientSignInRequest.new(login: login, password: password).execute
     end
 
     def sign_out(token)
+      Rails.cache.delete(:api_token) if Rails.cache
       Smartcore::ClientSignOutRequest.new(api_token: token).execute
     end
 
     def request_token
-      if Rails.cache
-        unless Rails.cache.exist?(:api_token)
-          Rails.cache.write(:api_token, sign_in(self.login, self.password))
-        end
-        Rails.cache.read(:api_token, self.token)
-      else
-        sign_in(self.login, self.password)
-      end
+      token = sign_in(self.login, self.password)
+      Rails.cache.write(:api_token, token) if Rails.cache
+      token
     end
   end
 end
