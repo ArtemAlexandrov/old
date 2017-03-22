57502e660908a6cfb9f631558de226de2b404041
 Artem Alexandrov <svarog278@gmail.com>
2017-03-22 15:59:30 +0300

    error on registrations

--- lib/smartcore/requests/user_profile_requests/user_profile_registration_request.rb
+++ lib/smartcore/requests/user_profile_requests/user_profile_registration_request.rb
@@ -16,5 +16,19 @@ module Smartcore
     def path
       router.user_create_path
     end
+
+    def process_error(response)
+      if response.status == 422
+        response_json = JSON.parse(response.body)
+        if response_json['error'] == 'validation_error'
+          profile = Smartcore::Models::User.new(response_json['profile'])
+          profile.init_errors_by_json(response_json['messages'])
+          profile
+        else
+          super(response)
+        end
+      end
+    end
+
   end
 end
--- lib/smartcore/version.rb
+++ lib/smartcore/version.rb
@@ -1,3 +1,3 @@
 module Smartcore
-  VERSION = "0.1.9"
+  VERSION = "0.1.10"
 end
